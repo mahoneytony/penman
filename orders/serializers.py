3a875e10d8b9b8a5930a13304a6ff745b4d43fa2
Sasha <handsome.macaco@gmail.com>
2018-11-15 22:49:57 +0200

    fixed create func in views

--- orders/serializers.py
+++ orders/serializers.py
@@ -4,6 +4,3 @@ from .models import Order
 class OrderSerializer(serializers.Serializer):
     subject = serializers.CharField(max_length=300)
     topic = serializers.CharField(max_length=1000)
-
-    def create(self, validated_data):
-        return Order.objects.create(**validated_data)
--- orders/views.py
+++ orders/views.py
@@ -2,24 +2,21 @@
 from __future__ import unicode_literals
 
 from django.shortcuts import render
-from rest_framework import viewsets
+from rest_framework import viewsets, status
 from rest_framework.response import Response
 
 from .models import Order
 from .serializers import OrderSerializer
 
-class OrderViewSet(viewsets.ModelViewSet):
+class OrderViewSet(viewsets.ViewSet):
     queryset = Order.objects.all()
     serializer_class = OrderSerializer
 
-    # def create(self, request, *args, **kwarg):
-    #     serializer = self.serializer_class(data=request.data)
-    #     if serializer.is_valid():
-    #         Order.objects.create(serializer.validated_data)
-    #         return Response(serializer.validated_data,
-    #                status=status.HTTP_201_CREATED)
-    #     else:
-    #         return Response(serializer.errors)
-
-    # def perform_create(self, serializer):
-    #    serializer.save()
+    def create(self, request, *args, **kwargs):
+        serializer = self.serializer_class(data=request.data)
+        if serializer.is_valid():
+            Order.objects.create(**serializer.validated_data)
+            return Response(serializer.validated_data,
+                   status=status.HTTP_201_CREATED)
+        else:
+            return Response(serializer.errors)
